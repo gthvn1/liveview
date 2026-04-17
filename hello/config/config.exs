eb800a61ef96fdac3710dcc209893a107699cc30
Guillaume <gthvn1@gmail.com>
2026-04-17 16:46:28 +0200

    Fix path for tailwindcss and esbuild

--- hello/config/config.exs
+++ hello/config/config.exs
@@ -32,7 +32,8 @@ config :hello, Hello.Mailer, adapter: Swoosh.Adapters.Local
 
 # Configure esbuild (the version is required)
 config :esbuild,
-  version: "0.25.4",
+  version: "0.25.5",
+  path: System.get_env("ESBUILD_PATH") || System.find_executable("esbuild"),
   hello: [
     args:
       ~w(js/app.js --bundle --target=es2022 --outdir=../priv/static/assets/js --external:/fonts/* --external:/images/* --alias:@=.),
@@ -42,7 +43,8 @@ config :esbuild,
 
 # Configure tailwind (the version is required)
 config :tailwind,
-  version: "4.1.12",
+  version: "4.1.18",
+  path: System.get_env("TAILWINDCSS_PATH") || System.find_executable("tailwindcss"),
   hello: [
     args: ~w(
       --input=assets/css/app.css
